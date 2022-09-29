a3cf102095ace8d99155b0c9b2bd0f484f71fbcd
Luka Gurdulic <gurdulicluka@gmail.com>
2022-09-30 00:06:08 +0200

    optional chaining reformat on some fetch data

--- src/Banner.js
+++ src/Banner.js
@@ -18,6 +18,7 @@ function Banner() {
     }
     fetchData();
   }, []);
+  console.log(movie);
 
   const truncate = (string, n) => {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
@@ -33,17 +34,14 @@ function Banner() {
       }}
     >
       <div className="banner__content">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">
-          {truncate(
-            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, ad laudantium. Libero aspernatur quisquam est voluptates id numquam fuga praesentium qui pariatur illo? Minima doloremque vel optio voluptatem ea temporibus. `,
-            150
-          )}
-        </h1>
+        <h1 className="banner__description">{truncate(movie.overview, 150)}</h1>
       </div>
 
       <div className="banner--fadeBottom" />
