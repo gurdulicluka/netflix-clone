ff7400a518e948987c675a18ed141bc1e0dd8d6b
Luka Gurdulic <gurdulicluka@gmail.com>
2022-09-30 19:52:24 +0200

    movie category rows

--- src/Banner.js
+++ src/Banner.js
@@ -18,7 +18,6 @@ function Banner() {
     }
     fetchData();
   }, []);
-  console.log(movie);
 
   const truncate = (string, n) => {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
@@ -41,7 +40,9 @@ function Banner() {
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">{truncate(movie.overview, 150)}</h1>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
 
       <div className="banner--fadeBottom" />
