3692a2c82912e6f7b7d44ed5a753a170bc42d348
Luka Gurdulic <gurdulicluka@gmail.com>
2022-10-04 14:32:12 +0200

    firebase set and deployed
    Deployed for ease of future updates

--- src/Banner.js
+++ src/Banner.js
@@ -8,7 +8,7 @@ function Banner() {
 
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(requests.fetchNetflix0riginals);
+      const request = await axios.get(requests.fetchHorrorMovies);
       setMovie(
         request.data.results[
           Math.floor(Math.random() * request.data.results.length - 1)
